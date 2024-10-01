b0ef930dcf8930397daa556d75ec0ae5020f449d
jack <dancingvulture@gmail.com>
2024-09-30 21:17:36 -0700

    updated readme, modified shell scripts, changed name of gen.py to main.py

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 source ".venv/bin/activate"
 
-alias g="python3 gen.py"
+# Add any program specific aliases here
+alias run="python3 main.py"
 
-echo "Welcome to the D&D name generator!"
-echo "A useful shortcut: g = python3 gen.py"
+# Add any console welcome messages here
+
 
